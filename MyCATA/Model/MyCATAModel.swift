ace1e791ac9f93b2f86d0e165bdff0cd45e6207c
Punnavit Akkarapitakchai <pva5115@psu.edu>
2017-11-18 17:36:02 -0500

    add favorites and favorites support for tableview

--- MyCATA/Model/MyCATAModel.swift
+++ MyCATA/Model/MyCATAModel.swift
@@ -11,9 +11,12 @@ import Foundation
 class MyCATAModel {
     static let sharedInstance = MyCATAModel()
     
+    //MARK: - Properties
     let routeDetails : [RouteDetail]
     let routeNames : [String]
+    let routeIdToIndex : [Int: Int]
     let stops : [Stop]
+    var favorites : [Int] = []
     
     fileprivate init() {
         let fileManager = FileManager.default
@@ -26,13 +29,19 @@ class MyCATAModel {
         do {
             routeDetails = try decoder.decode([RouteDetail].self, from: data)
             var _routeNames = [String]()
+            var _routeIdToIndex = [Int: Int]()
+            var index = 0
             for routeDetail in routeDetails {
                 _routeNames.append(routeDetail.longName)
+                _routeIdToIndex[routeDetail.routeId] = index
+                index += 1
             }
             routeNames = _routeNames
+            routeIdToIndex = _routeIdToIndex
         } catch let error as NSError {
             routeDetails = []
             routeNames = []
+            routeIdToIndex = [:]
             print("Unresolved Error \(String(describing: error)))" )
         }
         
@@ -47,7 +56,7 @@ class MyCATAModel {
         }
     }
     
-    //Support for TableView
+    //MARK: - Support for TableView
     var numberOfRoutes : Int { return routeDetails.count }
     
     func route(forIndexPath indexPath: IndexPath) -> RouteDetail {
@@ -60,4 +69,32 @@ class MyCATAModel {
         return routeNames[index]
     }
     
+    func isFavorite(indexPath: IndexPath) -> Bool {
+        let id = route(forIndexPath: indexPath).routeId
+        return favorites.contains(id)
+    }
+    
+    func addToFavorite(indexPath: IndexPath) -> Bool {
+        guard favorites.count < 3 else { return false }
+        let id = route(forIndexPath: indexPath).routeId
+        favorites.append(id)
+        return true
+    }
+    
+    func removeFromFavorite(indexPath: IndexPath) {
+        let id = route(forIndexPath: indexPath).routeId
+        if let index = favorites.index(of: id) {
+            favorites.remove(at: index)
+        }
+    }
+    
+    func getFavoriteIndices() -> [IndexPath] {
+        var indices = [IndexPath]()
+        for id in favorites {
+            let index = routeIdToIndex[id]
+            let indexPath = IndexPath(row: index, section: 1)
+            indices.append(indexPath)
+        }
+        return indices
+    }
 }
