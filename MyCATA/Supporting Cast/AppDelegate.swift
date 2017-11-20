b40feda4dd25ae7c76b34d35c766ba89f25d5b81
Punnavit Akkarapitakchai <pva5115@psu.edu>
2017-11-19 23:26:33 -0500

    add userdefaults

--- MyCATA/Supporting Cast/AppDelegate.swift
+++ MyCATA/Supporting Cast/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let defaults = UserDefaults.standard
+        defaults.register(defaults: [UserDefaultsKeys.firstLaunch:true])
+        defaults.register(defaults: [UserDefaultsKeys.favorites:[]])
+        
         return true
     }
 
