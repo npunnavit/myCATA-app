7f5b84d342f349715befa4d88fe3541151530d22
Punnavit Akkarapitakchai <pva5115@psu.edu>
2017-11-19 23:24:12 -0500

    add persistance and request for location service

--- MyCATA/Controller/WelcomeViewController.swift
+++ MyCATA/Controller/WelcomeViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import MapKit
 
 class WelcomeViewController: UIViewController {
     
     let model = MyCATAModel.sharedInstance
-
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = UserDefaults.standard
+        defaults.set(false, forKey: UserDefaultsKeys.firstLaunch)
+        defaults.synchronize()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if CLLocationManager.locationServicesEnabled() {
+            let status = CLLocationManager.authorizationStatus()
+            switch status {
+            case .notDetermined, .denied:
+                locationManager.requestWhenInUseAuthorization()
+            default:
+                break
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
