1549cbec34321b36e08e8462af9d2e336d6d4b23
Punnavit Akkarapitakchai <pva5115@psu.edu>
2017-12-01 01:39:57 -0500

    showing bus on route map View in progress

---
+++ MyCATA/Model/Class and Struct/VehicleLocation.swift
@@ -0,0 +1,9 @@
+//
+//  VehicleLocation.swift
+//  MyCATA
+//
+//  Created by Punnavit Akkarapitakchai on 11/30/17.
+//  Copyright © 2017 Punnavit Akkarapitakchai. All rights reserved.
+//
+
+import Foundation
